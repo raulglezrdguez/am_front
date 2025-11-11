5ebb576939de02cb8747c4220e50c9732ca7a708
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-11-11 13:12:33 -0500

    Lang type added

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,12 +1,14 @@
+import type { Lang } from "@/types/languages";
 import { getDictionary } from "./dictionaries";
 
 export default async function Home({
   params,
 }: {
-  params: Promise<{ lang: "en" | "es" | "fr" | "it" | "de" }>;
+  params: Promise<{ lang: Lang }>;
 }) {
   const { lang } = await params;
   const dict = await getDictionary(lang);
+
   return (
     <div className="flex min-h-screen items-center justify-center font-sans text-white">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
