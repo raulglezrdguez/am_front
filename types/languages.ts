97d90045411f60a81e254ac8f996985aba7e72da
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-11-14 11:56:14 -0500

    languages type updated

--- types/languages.ts
+++ types/languages.ts
@@ -1,3 +1,29 @@
 export type Lang = "en" | "es" | "fr" | "it" | "de";
 
 export const SUPPORTED_LOCALES = ["en", "es", "fr", "it", "de"] as const;
+
+export type Dictionary = {
+  metadata: {
+    description: string;
+  };
+  header: {
+    exam: string;
+    patient: string;
+    stat: string;
+    auth: string;
+    closeSession: string;
+  };
+  login: {
+    title: string;
+    emailPlaceholder: string;
+    passwordPlaceholder: string;
+    emailLoginButton: string;
+    orLoginWith: string;
+    googleLoginButton: string;
+    error: string;
+    accountExistsError: string;
+    popupBlockedError: string;
+    googleLoginError: string;
+  };
+  loading: string;
+};
