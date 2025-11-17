ef8c3af3a29255e00d8574745d42ebf3641aa52d
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-11-17 17:37:51 -0500

    GET_EXAM & getExam & UPDATE_EXAM_MUTATION added

--- lib/services/graphql/queries/examQueries.ts
+++ lib/services/graphql/queries/examQueries.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 import { cookies } from "next/headers";
 import { createServerApolloClient } from "../serverClient";
-import { ExamsResponse } from "@/lib/types/exam";
+import { ExamResponse, ExamsResponse } from "@/lib/types/exam";
 
 export const GET_EXAMS = gql`
   # query GetExams($limit: Int = 20, $offset: Int = 0) {
@@ -50,6 +50,51 @@ export const GET_EXAMS = gql`
   }
 `;
 
+export const GET_EXAM = gql`
+  query GetExam($id: ID!) {
+    exam(id: $id) {
+      _id
+      title
+      subtitle
+      instructions
+      description
+      public
+      year
+      author {
+        name
+        email
+      }
+      expression {
+        id
+        operator
+        value
+        label
+        reference
+        variable
+      }
+      questions {
+        id
+        text
+        expression {
+          id
+          operator
+          value
+          label
+          reference
+          variable
+        }
+        answer
+        reference
+        answers {
+          id
+          value
+          content
+        }
+      }
+    }
+  }
+`;
+
 export const CREATE_EXAM_MUTATION = gql`
   mutation CreateExam($input: CreateExamInput!) {
     createExam(input: $input) {
@@ -62,6 +107,51 @@ export const CREATE_EXAM_MUTATION = gql`
       author {
         name
         email
+      }
+    }
+  }
+`;
+
+export const UPDATE_EXAM_MUTATION = gql`
+  mutation UpdateExam($id: ID!, $input: UpdateExamPropertiesInput!) {
+    updateExam(id: $id, input: $input) {
+      _id
+      title
+      subtitle
+      instructions
+      description
+      public
+      year
+      author {
+        name
+        email
+      }
+      expression {
+        id
+        operator
+        value
+        label
+        reference
+        variable
+      }
+      questions {
+        id
+        text
+        expression {
+          id
+          operator
+          value
+          label
+          reference
+          variable
+        }
+        answer
+        reference
+        answers {
+          id
+          value
+          content
+        }
       }
     }
   }
@@ -87,6 +177,44 @@ export async function getExams() {
     const exams = data?.exams || [];
 
     return { exams };
+  } catch (error) {
+    const errorObj = error as Record<string, unknown>;
+    if (errorObj.code === "auth/argument-error") {
+      return { exams: [], error: { message: "Invalid session", status: 401 } };
+    }
+
+    return {
+      exams: [],
+      error: {
+        message: (error as Error).message || "Internal server error",
+        status: 500,
+      },
+    };
+  }
+}
+
+export async function getExam(id: string) {
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("session")?.value;
+
+    // Create Apollo client with token (if available)
+    const serverClient = createServerApolloClient(token);
+
+    const { data } = await serverClient.query<ExamResponse>({
+      query: GET_EXAM,
+      context: {
+        fetchOptions: {
+          next: { revalidate: 300 },
+        },
+      },
+      variables: { id },
+    });
+
+    console.log(data);
+    const exam = data?.exam || null;
+
+    return { exam };
   } catch (error) {
     const errorObj = error as Record<string, unknown>;
     if (errorObj.code === "auth/argument-error") {
